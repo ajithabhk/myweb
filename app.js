94e929d75937db053fd0ac833632f5b95de429b9
Ajithabh Krishna <90406707+ajithabhk@users.noreply.github.com>
2023-09-16 15:39:39 +0530

    app.js

--- app.js
+++ app.js
@@ -11,3 +11,10 @@
         document.body.classList.toggle("light-mode");
     })
 })();
+window.addEventListener("load", ()=>{
+    const loader =document.querySelector(".loader");
+    loader.classList.add("loader-hidden");
+    loader.addEventListener("transitionend",()=>{
+        document.body.removeChild("loader");
+    })
+});
